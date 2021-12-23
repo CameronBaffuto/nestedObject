e11a68f4643474f6c0218279616c672becf17978
Cam <cameron.baffuto@bionical.com>
2021-12-23 15:05:57 -0500

    init

---
+++ data.js
@@ -0,0 +1,52 @@
+const testData = [
+    {
+        title: "1",
+        comment: [
+            {
+                name: "Cam",
+                message: "Hi this is Cam comment",
+                amount: "$280",
+            },
+            {
+                name: "Lauren",
+                message: "This is Lauren",
+                amount: "$151",
+            }
+          ]
+    },
+    {
+        title: "2",
+        comment: [
+            {
+                name: "Lauren 2",
+                message: "This is Lauren 2",
+                amount: "$158",
+            },
+            {
+                name: "Cam 2",
+                message: "Hi this is Cam 2 comment",
+                amount: "$270",
+            },
+            {
+                name: "Tester",
+                message: "By Golly",
+                amount: "$999",
+            }
+          ]
+    },
+    {
+        title: "3",
+        comment: [
+            {
+                name: "Lauren 3",
+                message: "This is Lauren 3",
+                amount: "$95",
+            },
+            {
+                name: "Cam 3",
+                message: "Hi this is Cam 3 comment",
+                amount: "$27",
+            }
+          ]
+    },
+];
