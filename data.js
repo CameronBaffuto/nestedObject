6f6745cf7f70884d76c0de6a31c4fa500be2e769
cameronbaffuto <cb3426@drexel.edu>
2021-12-24 17:18:40 -0500

    add total

--- data.js
+++ data.js
@@ -4,49 +4,53 @@ const testData = [
         comment: [
             {
                 name: "Cam",
-                message: "Hi this is Cam comment",
-                amount: "$280",
+                message: "Hi this is Cam comment. Hi this is Cam comment. Hi this is Cam comment. Hi this is Cam comment. Hi this is Cam comment. Hi this is Cam comment.",
+                amount: "280",
             },
             {
                 name: "Lauren",
-                message: "This is Lauren",
-                amount: "$151",
+                message: "This is Lauren. This is Lauren. This is Lauren. This is Lauren. This is Lauren. This is Lauren. This is Lauren. This is Lauren. This is Lauren.",
+                amount: "151",
             }
-          ]
+          ],
+        total: "431",  
     },
     {
         title: "2",
         comment: [
             {
                 name: "Lauren 2",
-                message: "This is Lauren 2",
-                amount: "$158",
+                message: "This is Lauren 2. This is Lauren 2. This is Lauren 2. This is Lauren 2. This is Lauren 2. This is Lauren 2. This is Lauren 2. This is Lauren 2. ",
+                amount: "158",
             },
             {
                 name: "Cam 2",
-                message: "Hi this is Cam 2 comment",
-                amount: "$270",
+                message: "Hi this is Cam 2 comment. Hi this is Cam 2 comment. Hi this is Cam 2 comment. Hi this is Cam 2 comment. Hi this is Cam 2 comment. Hi this is Cam 2 comment.",
+                amount: "270",
             },
             {
                 name: "Tester",
-                message: "By Golly",
-                amount: "$999",
+                message: "By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly. By Golly",
+                amount: "999",
             }
-          ]
+          ],
+          total: "1427",
     },
     {
         title: "3",
         comment: [
             {
                 name: "Lauren 3",
-                message: "This is Lauren 3",
-                amount: "$95",
+                message: "This is Lauren 3. This is Lauren 3. This is Lauren 3. This is Lauren 3. This is Lauren 3. This is Lauren 3. This is Lauren 3. This is Lauren 3. This is Lauren 3.",
+                amount: "95",
             },
             {
                 name: "Cam 3",
-                message: "Hi this is Cam 3 comment",
-                amount: "$27",
+                message: "Hi this is Cam 3 comment. Hi this is Cam 3 comment. Hi this is Cam 3 comment. Hi this is Cam 3 comment. Hi this is Cam 3 comment. Hi this is Cam 3 comment. Hi this is Cam 3 comment.",
+                amount: "27",
             }
-          ]
+          ],
+          total: "122",
     },
+    
 ];
